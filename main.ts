5e0978128c46f50a889407cb625f40728dec588d
domekisuzi <domekisuzi@163.com>
2024-06-03 13:27:13 +0800

    初次使用，有很多bug

--- main.ts
+++ main.ts
@@ -1,134 +1,59 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {Notice, Plugin} from 'obsidian';
+import { HighlightModal } from './HighlightModal';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+export default class HighlightSaver extends Plugin {
+	onload(): void {
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'open-save-highlights-modal',
+			name: 'Open Save Highlights Modal',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+				const modal = new HighlightModal(this.app, () => this.saveHighlights());
+				modal.open();
+			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	async saveHighlights(): Promise<void> {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (!activeFile) {
+			new Notice('No active file found.');
+			return;
+		}
 
+		const fileContents = await this.app.vault.read(activeFile);
+		const highlights = fileContents.match(/\=\=(.*?)\=\=/g) || [];
+		if (highlights.length === 0) {
+			new Notice('No highlights found.');
+			return;
+		}
+
+		// Prepare table headers based on the number of columns in the first highlight
+		const firstHighlightColumns = highlights[0].replace(/==/g, '').trim().split('|').length;
+		const headers = '| ' + ' | '.repeat(firstHighlightColumns).trim() + ' |\n';
+		const separator = '| ' + '--- | '.repeat(firstHighlightColumns).trim() + ' |\n';
+
+		const tableContent = highlights.map(h => {
+			const parts = h.replace(/==/g, '').trim().split('|').map(part => part.trim()).join(' | ');
+			return `| ${parts} |`;
+		}).join('\n');
+
+		const newFilePath = activeFile.path.replace(/\..+$/, '-highlights.md');
+
+		// Save the content with headers
+		await this.app.vault.create(newFilePath, `# Highlights\n${headers}${separator}${tableContent}`);
+
+		// Create a bidirectional link
+		const originalFileName = activeFile.basename;
+		const highlightFileName = newFilePath.slice(newFilePath.lastIndexOf('/') + 1, newFilePath.lastIndexOf('.'));
+		const backLinkToOriginal = `[[${originalFileName}]]`;
+		const linkToHighlights = `[[${highlightFileName}]]`;
+
+		// Append links to both files
+		await this.app.vault.append(activeFile.path, `\n\nLink to highlights: ${linkToHighlights}`);
+		await this.app.vault.append(newFilePath, `\n\nBacklink to original file: ${backLinkToOriginal}`);
+
+		new Notice('Highlights saved to ' + newFilePath + ' with links.');
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
 }
